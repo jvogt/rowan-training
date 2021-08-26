2e06e6587fd9e1467cd6fed424142bd43fc3a45a
Jeff Vogt <jeffvogt@Jeff-Vogts-MacBook-Pro.local>
2021-08-25 20:27:31 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var formPhrase = require('font-ascii').default;
+
+app.get('/hello-world', function (req, res) {
+  console.log(req.headers)
+  res.end( formPhrase('Awesome Phrase') + '\n' );
+})
+
+app.get('/six-months', function (req, res) {
+  provided_date = req.query['date']
+  formatted_date = new Date(provided_date)
+  console.log(formatted_date)
+  six_months_later = new Date(formatted_date.setMonth(formatted_date.getMonth()+6));
+  res.end( 'you will die in ' + six_months_later + '\n' );
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
